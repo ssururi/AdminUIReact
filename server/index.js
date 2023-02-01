2bbb6d1f9682d9ed1a41ad411393b0d2f4fe8464
Muhamad Shofyan Sururi <muhamad.sururi@interport.co.id>
2023-02-01 07:10:20 +0700

    Add theme

--- server/index.js
+++ server/index.js
@@ -10,6 +10,22 @@ import morgan from "morgan";
 // import managementRoutes from "./routes/management.js";
 // import salesRoutes from "./routes/sales.js";
 
+// data imports
+import User from "./models/User.js";
+// import Product from "./models/Product.js";
+// import ProductStat from "./models/ProductStat.js";
+// import Transaction from "./models/Transaction.js";
+// import OverallStat from "./models/OverallStat.js";
+// import AffiliateStat from "./models/AffiliateStat.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./data/index.js";
+
 /* CONFIGURATION */
 dotenv.config();
 const app = express();
