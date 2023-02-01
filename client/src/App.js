7cc27fba9566d2ab963c0b137df1bb8245962659
MSSururi <wibisono.indrawan@kapalapi.co.id>
2023-02-01 17:33:39 +0700

    Add Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,26 @@ import createTheme from "@mui/material";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { themeSettings } from "theme";
+import { BrowserRouter } from "react-router-dom";
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />}/>
+              <Route path="/dashboard" element={<Dashboarrd />}/>
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
